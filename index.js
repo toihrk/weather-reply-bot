c320422e91c50d9aa704278242939d1bccb2b77e
toihrk <hiroki_toi@hitoi.local>
2015-04-15 16:55:24 +0900

    :put_litter_in_its_place:

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
   fs.statSync("./config.json");
   config = require('./config.json');
 } catch (e) {
-  console.log("no config.json");
+  console.log("No config.json");
 }
 
 var account = "@"+ (process.env.account || config.account);
@@ -24,53 +24,27 @@ var client = new twitter({
 
 var weather = request.createClient('http://api.openweathermap.org/');
 
-function icon_url(id) {
+function iconUrl(id) {
   return "http://openweathermap.org/img/w/"+id+".png"
 }
 
-// stream.on ->
-//    replyTrigger(tweet:obj) ->
-//       location(post:obj) ->
-//          getWeather(post:obj) ->
-//             update(post:obj)
-
-// post : {
-//   "user" : "@~~~",
-//   "id"   : "~~~", # in_reply_to_status_id
-//   "text" : "東京",
-//   "loc"  : {
-//     "lat" : "35.681382",
-//     "lng" : "139.766084"
-//   },
-//   "reply" : "scattered clouds http://openweathermap.org/img/w/03n.png"
-// }
-
-
 function replyTrigger(tweet) {
   if (tweet.text.indexOf(account) != -1) {
-  //if (tweet.text.indexOf("@") != -1) {
     var text = tweet.text.replace(account, "");
-    //var text = tweet.text.replace("@", "");
     text = text.replace(" ", "").replace("　", "");
-    console.log(tweet);
     var post = {
       "user" : "@"+tweet.user.screen_name,
       "id"   : tweet.id,
       "text" : text,
       "reply" : ""
     };
-
     location(post);
-
   }
 }
-
-
 
 function location(post) {
   var _post = post;
   maps.geocode(post.text, function(err, data) {
-    console.log(data);
     if(data.status == "OK") {
       _post.loc = {};
       _post.loc = data.results[0].geometry.location;
@@ -86,12 +60,10 @@ function location(post) {
 function getWeather(post) {
   var _post = post;
   weather.get("data/2.5/weather?lat="+_post.loc.lat+"&lon="+_post.loc.lng, function(err, res, body) {
-    console.log(body);
     if(body.cod==200){
         var desc = body.weather[0].description;
         var icon = body.weather[0].icon;
-        _post.reply = desc + " " + icon_url(icon);
-
+        _post.reply = desc + " " + iconUrl(icon);
         update(_post);
     }
   });
@@ -105,33 +77,19 @@ function update(post) {
     "in_reply_to_status_id": _post.id
   };
   if(_post.loc) {
-      status.lat  = _post.loc.lat;
-      status.long = _post.loc.lng;
       status.status += " " + "http://maps.google.com/maps?q="+_post.loc.lat+","+_post.loc.lng;
   }
-  console.log(status);
   client.post('statuses/update', status, function(err, tweet, res) {
     if (!err) {
-      console.log(tweet);
+      console.log("Tweeted: "+tweet.text);
     }
   });
 }
 
 client.stream('user', function(stream) {
-
-//client.stream('statuses/sample', function(stream) {
   stream.on('data', function(tweet) {
     if(tweet.text) {
       replyTrigger(tweet);
-      //console.log(tweet);
     }
   });
 });
-
-// replyTrigger({
-//   "text": "@weather_reply 東京",
-//   "id"  : "587934109785141248",
-//   "user": {
-//     "screen_name": "toihrk"
-//   }
-// });
